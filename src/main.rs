e4bb54c26540440097324968948347f0bda9b2fe
0xh4ty <0xh4ty@proton.me>
2026-05-12 11:19:46 +0530

    feat: make footer copyright year configurable

--- src/main.rs
+++ src/main.rs
@@ -32,6 +32,7 @@ struct SiteInfo {
     author_glitch_effect: bool,
     profile_picture: String,
     base_url: String,
+    copyright_year: String,
     og_image: Option<String>,
 }
 
@@ -403,7 +404,7 @@ fn wrap_in_template(
         <a href='{twitter}'><svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round' class='feather feather-twitter'><path d='M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z'></path></svg></a>
     </p>
     <p class='footer-credit'>
-        © 2025 <a href='{twitter}'>{author}</a>. Powered by <a href='https://github.com/0xh4ty/xeniria'>Xeniria</a>.
+        © {copyright_year} <a href='{twitter}'>{author}</a>. Powered by <a href='https://github.com/0xh4ty/xeniria'>Xeniria</a>.
     </p>
 </footer>
 <script>
@@ -422,7 +423,8 @@ fn wrap_in_template(
         body_content = body_content,
         author = config.site.author,
         github = config.links.github,
-        twitter = config.links.twitter
+        twitter = config.links.twitter,
+        copyright_year = config.site.copyright_year,
     )
 }
 
